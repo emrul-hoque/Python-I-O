28b46d6d6aaf7663ca1f4e1ab8ec7d15e789e8ed
Emrul <emrul.hoque@gmail.com>
2024-01-15 19:44:50 +0600

    initial commit

---
+++ total_words.py
@@ -0,0 +1,19 @@
+import os
+
+os.system("cls")
+
+file_path = "F:\Emrul\Python\Python IO\Python IO.txt"
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        word_count = len(content.split())
+        print("Total number of words in the sentence is:\t", word_count)
+        #print("\n",content)
+except FileNotFoundError:
+    print(f"The file {file_path} was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+print()
+
